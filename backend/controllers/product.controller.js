1633f13f4c2dacb5eb3a2389d1f4f928d758edf4
Wangu <2101287@sit.singaporetech.edu.sg>
2023-10-29 15:11:30 +0800

    product update and create new product, error message updated

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -50,12 +50,12 @@ exports.createProduct = async (req, res) => {
     //Input Validation function call for title
     if (!isValidTitleCheck(req.body)) {
       return res.status(400).json({
-        message: "Invalid input data on title! Please no use $.{}=",
+        message: "Invalid input data on title! Please no use $.{}=;",
         success: false,
       });
     } else if (!isValidDescriptionCheck(req.body)) {
       return res.status(400).json({
-        message: "Invalid input data on description! Please no use $.{}=",
+        message: "Invalid input data on description! Please no use $.{}=;",
         success: false,
       });
     } else if (!isValidPriceCheck(req.body)) {
@@ -125,12 +125,12 @@ exports.updateProduct = async (req, res) => {
     //Input Validation function call for title
     if (!isValidTitleCheck(req.body)) {
       return res.status(400).json({
-        message: "Invalid input data on title! Please no use $.{}=",
+        message: "Invalid input data on title! Please no use $.{}=;",
         success: false,
       });
     } else if (!isValidDescriptionCheck(req.body)) {
       return res.status(400).json({
-        message: "Invalid input data on description! Please no use $.{}=",
+        message: "Invalid input data on description! Please no use $.{}=;",
         success: false,
       });
     } else if (!isValidPriceCheck(req.body)) {
